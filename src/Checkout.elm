c61d4a3abf7df4af889b97e634abde4473769a4e
Robbie McKinstry <rem89@pitt.edu>
2016-07-22 12:58:33 -0400

    Adding comments

--- src/Checkout.elm
+++ src/Checkout.elm
@@ -1,4 +1,15 @@
-module Stripe exposing (checkout, Model)
+module Checkout exposing (checkout, Model)
+
+{-| Wrapper for the Stripe Checkout embedded form. See [this link](https://stripe.com/docs/checkout/tutorial) for the official documentation
+
+# Data
+@docs Model 
+Model is A simple structural type that contains all of the information passed as data-attributes to Stripe checkout script.
+        
+@docs checkout 
+checkout is the function that, taking a model, returns the HTML configured with that model. It will be the "Pay with Card" button from the above linked tutorial.
+
+-}
 
 import Html exposing (Attribute, form, Html, node)
 import Html.Attributes exposing ( attribute, src, id, class, async)
@@ -6,6 +17,11 @@ import Html.Attributes exposing ( attribute, src, id, class, async)
 
 -- View
 
+{-|
+
+@docs Model is A simple structural type that contains all of the information passed as data-attributes to Stripe checkout script.
+ 
+-}
 type alias Model = 
     {
         key:            String
@@ -19,6 +35,10 @@ script : List (Attribute msg) -> List (Html msg) -> Html msg
 script attributes children =
     node "script" attributes children
 
+{-|
+@docs checkout 
+checkout is the function that, taking a model, returns the HTML configured with that model. It will be the "Pay with Card" button from the above linked tutorial.
+-}
 checkout: Model -> Html a
 checkout model = 
     form [ id "stripe-form"]
